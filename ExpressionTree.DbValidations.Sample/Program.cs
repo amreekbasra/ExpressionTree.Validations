980db67c63d61c42c08dc7c9bd2cd050ae2a8597
amreekbasra <amreekbasra@gmail.com>
2019-11-06 20:23:27 +0530

    first commit

---
+++ ExpressionTree.DbValidations.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpressionTree.DbValidations.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World I am there!");
+        }
+    }
+}
